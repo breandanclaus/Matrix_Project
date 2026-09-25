23b2f55bf9e87a5d79af4c83b16d0f66a3712c91
breandan.claus <breandan.claus@MU-C1-02.mullen.pvt.k12.co.us>
2026-09-25 11:54:39 -0600

    Initial Commit
    Grid and working on prepare grid

---
+++ Matrix Project/src/MatrixProject.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class MatrixProject
+	{
+		static String[][] grid = new String[6][5];
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+				displayBoard();
+
+			}
+		
+		public static String prepareBoard()
+		{
+			
+		}
+
+		private static void displayBoard()
+			{
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+				System.out.println("|     |     |     |     |     |");
+				System.out.println("-------------------------------");
+			}
+
+	}
---
+++ Matrix Project/src/WordList.java
@@ -0,0 +1,11 @@
+
+public class WordList
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+	}
